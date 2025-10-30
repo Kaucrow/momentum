ed92770647111aa81a0d70ccba0dce29f15d4b23
Sebangel <angeljavicueche@gmail.com>
2025-10-29 20:05:08 -0400

    Feat: Task manager uses function to get elements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,58 @@
 <script lang="ts">
   import Button from "$lib/components/ui/button/button.svelte";
+  import { onMount } from "svelte";
   import * as Accordion from "$lib/components/ui/accordion/index.js";
   import { Checkbox } from "$lib/components/ui/checkbox/index.js";
   import { Label } from "$lib/components/ui/label/index.js";
+
+  type AccordionItem = {
+    label: string;
+    description: string;
+    date: string;
+  };
+
+  let items : AccordionItem[] = []
+
+  function getItems(){
+    let tempItems = [{label: "Terminar el proyecto de componentes",
+      description: "Exposición y Proyecto",
+      date: "2025/10/30" 
+      },
+      {label: "Enfrentar mi vida",
+      description: "Evitar la automuerte",
+      date: "Everyday" 
+    }]
+    items=tempItems;
+  }
+
+  function deleteItem(index:number){
+    //items[index]
+  }
+
+  onMount(() => {
+    getItems();
+  });
 </script>
 
-<Button>Elatla</Button>
+<h1 class="text-center text-7xl m-10">Task Manager</h1>
 
-<Accordion.Root type="single" class="w-full sm:max-w-[70%] ml-3" value="item-1">
-  <Accordion.Item value="item-1" class="border-2 border-gray">
-    <div class="flex items-center gap-3 ml-3"> 
-      <Checkbox id="terms" />
-      <Accordion.Trigger id="trigger">
-        <Label for="trigger">Accept terms and conditions</Label>
-      </Accordion.Trigger>
-    </div>
-    <Accordion.Content class="flex flex-col gap-4 text-balance">
-      <p>
-        Our flagship product combines cutting-edge technology with sleek design.
-        Built with premium materials, it offers unparalleled performance and
-        reliability.
-      </p>
-      <p>
-        Key features include advanced processing capabilities, and an intuitive
-        user interface designed for both beginners and experts.
-      </p>
-    </Accordion.Content>
-  </Accordion.Item>
-
-  <Accordion.Item></Accordion.Item>
-  
-</Accordion.Root>
+<div class="flex items-center h-screen flex-col">
+  <Accordion.Root type="multiple" class="w-full sm:max-w-[21%] ml-3">
+    {#each items as item, index}
+      <Accordion.Item value={`item-${index}`} class="border-gray">
+        <div class="flex items-center gap-3 ml-3"> 
+          <Checkbox id="terms"  onCheckedChange="{() => deleteItem(index)}"/>
+          <Accordion.Trigger id="trigger-{index}">
+            <Label for="trigger-{index}">{item.label}</Label>
+          </Accordion.Trigger>
+        </div>
+        <Accordion.Content class="flex flex-col gap-4 text-balance ml-3">
+          <p>{item.description}</p>
+          <p>
+            Limit date: {item.date}
+          </p>
+        </Accordion.Content>
+      </Accordion.Item>
+    {/each} 
+  </Accordion.Root>
+</div>
